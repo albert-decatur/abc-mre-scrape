7aa3ff16a332f6ff9bea1d453517c856cfa54fd9
Albert Decatur <adecatur@aiddata.org>
2014-07-24 17:02:45 -0400

    add todo / change get_pdf.sh to work on just one html file at a time

--- get_pdf.sh
+++ get_pdf.sh
@@ -5,8 +5,5 @@
 # must be saved as complete page
 # TODO - automate getting these
 
-find -type f -iregex ".*[.]html$" |\
-while read inhtml
-do 
-	cat $inhtml | grep -oE '<a href="http://pesquisa[.]in[.]gov[.]br/imprensa/jsp/visualiza/index[.]jsp[?]jornal=[^>]*' | sed 's:<a href="::g;s:"$::g' | sed 's:/jsp/visualiza/index.jsp:/servlet/INPDFViewer:g;s:$:\&captchafield=firistAccess:g'
-done
+inhtml=$1
+cat $inhtml | grep -oE '<a href="http://pesquisa[.]in[.]gov[.]br/imprensa/jsp/visualiza/index[.]jsp[?]jornal=[^>]*' | sed 's:<a href="::g;s:"$::g' | sed 's:/jsp/visualiza/index.jsp:/servlet/INPDFViewer:g;s:$:\&captchafield=firistAccess:g'
